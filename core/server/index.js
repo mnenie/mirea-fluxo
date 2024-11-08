9ea72c0dac9f12cad5c427bf88995e94d76c448d
airvt1x's PC <airat3552@gmail.com>
2024-11-08 15:35:19 +0300

    yo

--- core/server/index.js
+++ core/server/index.js
@@ -1,1 +1,32 @@
-console.log('123')
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+
+mongoose.set('strictQuery', false)
+
+dotenv.config()
+// eslint-disable-next-line node/prefer-global/process
+const MONGO_DB = process.env.MONGO_DB
+
+mongoose
+  .connect(MONGO_DB)
+  .then(() => console.log('DB ok'))
+  .catch(err => console.log('DB error', err))
+
+const app = express()
+app.use(express.json())
+
+app.listen(1234, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('Server OK ')
+})
+
+app.get('/', (req, res) => {
+  res.send('Гугуца батрачит')
+})
+// app.post('/auth/login', handleValidationErrors, UserController.login)
+// app.post('/auth/register', handleValidationErrors, UserController.register)
+// app.get('/auth/me', checkAuth, UserController.getMe)
---
+++ core/server/models/User.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+  },
+  photoUrl: {
+    type: String,
+  },
+}, {
+  timestamps: true,
+})
+
+export default mongoose.model('User', UserSchema)
