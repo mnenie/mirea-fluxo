15547cbbb98553167b22031f6657e94fb7b25e08
sv022 <ssvatkin04@gmail.com>
2024-11-10 19:38:50 +0300

    add: reviews table

--- core/client/src/components/reviews/card/Rating.vue
+++ core/client/src/components/reviews/card/Rating.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-row">
+  <div class="flex flex-row px-4">
     <span class="2xl:text-[11px] text-xs text-neutral-500">{{ stars }}/5 </span>
     <RatingStar class="w-2 h-2 text-blue-600" />
   </div>
