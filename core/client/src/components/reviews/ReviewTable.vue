7335368e5ff1b56487e4bbe38fb4eccee4d148e3
sv022 <ssvatkin04@gmail.com>
2024-11-11 14:20:54 +0300

    add: pagination for reviews table

--- core/client/src/components/reviews/ReviewTable.vue
+++ core/client/src/components/reviews/ReviewTable.vue
@@ -4,7 +4,19 @@ import { computed, onBeforeMount, ref, useTemplateRef } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { Calendar, Heading, Rating as RatingSvg, StatusTable, User } from '~/assets/svgs-vite'
 import { useReviewStore } from '~/stores/reviews'
+import type { Review } from '~/types/review.interface'
 import { Routes } from '~/utils/contants'
+import Button from '../ui/button/Button.vue'
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from '../ui/pagination/'
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 import {
   Table,
@@ -16,20 +28,19 @@ import {
 } from '../ui/table/'
 import ReviewRow from './table/ReviewRow.vue'
 
+const itemsPerPage = ref<number>(50)
+const currentPage = ref<number>(1)
+
 const reviewStore = useReviewStore()
 
-const el = ref<HTMLElement | null>(null)
+const reviewsPage = ref<Review[]>([])
 
-// const noNewReviews = ref<boolean>(false)
+function selectReviewPage(page: number, itemsPerPage: number) {
+  currentPage.value = page
+  reviewsPage.value = reviewStore.getPaginatedReviews(page, itemsPerPage)
+}
 
-// async function onLoadMore() {
-//   if (noNewReviews.value) {
-//     return
-//   }
-//   reviewStore.fetchReviews()
-// }
-
-// TODO: reactivity / obj link issue (возможно, создам репродюс чтобы решить и оптимизировать)
+selectReviewPage(1, itemsPerPage.value)
 
 const router = useRouter()
 const route = useRoute()
@@ -48,7 +59,7 @@ function toggleModalRoute() {
 
 // TODO (@sv022): надо поправить сайт из за того что бесконечно подругжается грузится на 587мБ ))
 // я думаю мб пагинацию ? https://www.shadcn-vue.com/docs/components/pagination.html
-// useInfiniteScroll(el, onLoadMore, { distance: 10 })
+// UPDATE: сделал пагинацию, можно удалять TODO
 
 const [DefineTemplate, ReuseTemplate] = createReusableTemplate()
 
@@ -59,7 +70,7 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div ref="el" class="overflow-y-auto h-full w-full bg-white shadow-sm rounded-md">
+  <div class="overflow-y-auto h-[calc(100dvh-125px)] w-full bg-white shadow-sm rounded-md">
     <Table>
       <TableCaption class="pb-4">
         A list of your recent invoices.
@@ -91,7 +102,13 @@ onBeforeMount(() => {
       <TableBody>
         <Sheet ref="sheet" v-model:open="isSheetOpen" @update:open="toggleModalRoute">
           <SheetTrigger as-child>
-            <ReviewRow v-for="review in reviewStore.reviews" :key="review.title" :review="review" class="cursor-pointer" @click="router.push(`/reviews/${review._id}`), isSheetOpen = true" />
+            <ReviewRow
+              v-for="review in reviewsPage"
+              :key="review.title"
+              :review="review"
+              class="cursor-pointer"
+              @click="router.push(`/reviews/${review._id}`), isSheetOpen = true"
+            />
           </SheetTrigger>
           <SheetContent class="min-w-[500px] !max-w-full w-1/3">
             <RouterView />
@@ -99,5 +116,27 @@ onBeforeMount(() => {
         </Sheet>
       </TableBody>
     </Table>
+    <Pagination v-slot="{ page }" :total="reviewStore.reviews.length / 5" :sibling-count="1" show-edges :default-page="1" class="m-2">
+      <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+        <PaginationFirst @click="selectReviewPage(1, itemsPerPage)" />
+        <PaginationPrev @click="selectReviewPage(currentPage - 1, itemsPerPage)" />
+
+        <template v-for="(item, index) in items">
+          <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+            <Button
+              class="w-10 h-10 p-0"
+              :variant="item.value === page ? 'default' : 'outline'"
+              @click="selectReviewPage(item.value, itemsPerPage)"
+            >
+              {{ item.value }}
+            </Button>
+          </PaginationListItem>
+          <PaginationEllipsis v-else :key="item.type" :index="index" />
+        </template>
+
+        <PaginationNext @click="selectReviewPage(currentPage + 1, itemsPerPage)" />
+        <PaginationLast @click="selectReviewPage(reviewStore.reviews.length / 50, itemsPerPage)" />
+      </PaginationList>
+    </Pagination>
   </div>
 </template>
--- core/client/src/stores/reviews.ts
+++ core/client/src/stores/reviews.ts
@@ -4,19 +4,31 @@ import type { Review } from '~/types/review.interface'
 
 export const useReviewStore = defineStore('reviews', () => {
   // TODO(@sv022): shallowRef вместо ref (после получения норм данных с бэка)
-  const reviews = ref<Review[]>([
-    { _id: '089798789', title: 'Hello world', content: 'content', email: '17bMw@example.com', date: new Date(), status: 'not verified', stars: 5 },
-    { _id: '1223809183', title: 'Привет я хз че я тут делаю', content: 'content', email: 'dasdsa@example.com', date: new Date(), status: 'verified', stars: 5 },
-  ])
+  const reviews = ref<Review[]>([])
+
+  // temporary for testing reviews pagination - remove later
+  for (let index = 0; index < 147; index++) {
+    reviews.value.push({ _id: `${index}`, title: 'Hello world', content: 'content', email: '17bMw@example.com', date: new Date(), status: 'not verified', stars: 5 })
+  }
 
   const fetchReviews = async function () {
     const response = await (await fetch('https://raw.githubusercontent.com/sv022/MockDB/refs/heads/main/ReviewService/reviews.json')).json()
     reviews.value.push(...response)
   }
 
+  const getPaginatedReviews = function (page: number, itemsPerPage: number) {
+    const paginatedReviews = <Review[]>[]
+    const pageEnd = Math.min(reviews.value.length, page * itemsPerPage)
+    for (let i = (page - 1) * itemsPerPage; i < pageEnd; i++) {
+      paginatedReviews.push(reviews.value[i])
+    }
+    return paginatedReviews
+  }
+
   return {
     reviews,
     fetchReviews,
+    getPaginatedReviews,
   }
 })
 
