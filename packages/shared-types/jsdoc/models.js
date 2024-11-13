f6e3745d531bd7d6df8ec516441ac555bb5cb9e6
danil <danila-kholkin@mail.ru>
2024-11-13 23:43:56 +0300

    change models.js

--- packages/shared-types/jsdoc/models.js
+++ packages/shared-types/jsdoc/models.js
@@ -1,42 +1,26 @@
 /**
  * @typedef {object} User
- * @property {string} _id - The unique identifier for the user.
- * @property {string} email - The email address of the user.
- * @property {string} photoUrl - The URL of the user's profile photo.
+ * @property {number} _id - Уникальный идентификатор пользователя
+ * @property {string} email - Адрес электронной почты пользователя
+ * @property {string} photoUrl - URL профиля пользователя
+ * @property {string} password - Пароль пользователя
+ * @property {'manager' | 'corporation'} role - Роль пользователя в системе, может быть менеджер или организация
+ * @property {string} organization - Название организации пользователя
  */
 
 /**
- * @typedef {object} Comment
- * @property {User} user - The user who created the comment.
- * @property {string} content - The content of the comment.
- * @property {Date} date - The date and time when the comment was created.
+ * @typedef {object} Orders
+ * @property {number} _id - Уникальный идентификатор заказа
+ * @property {string} title - Название заказа
+ * @property {string} content - Содержание заказа
+ * @property {Date} date - Дата и время создания заказа
+ * @property {'not verified' | 'in process' | 'closed'} status - Статус заказа, может быть не проверен, в процессе или закрыт
+ * @property {string} email - Адрес электронной почты заказчика
+ * @property {number} price - Цена заказа
+ * @property {string} organization - Название организации, ответственной за заказ
  */
 
 /**
  * @typedef {object} AI
- * @property {'user' | 'assistant'} role - The role of the AI.
- * @property {string | Promise<string} text - The text of the AI response.
- */
-
-/**
- * @typedef {object} Review
- * @property {string} _id - The unique identifier for the review.
- * @property {string} title - The title of the review.
- * @property {string} content - The content of the review.
- * @property {string} email - The email address of the user who created the review.
- * @property {Date} date - The date and time when the review was created.
- * @property {'not verified' | 'consideration' | 'verified'} status - The status of the review
- * @property {number} stars - The number of stars given to the review
- */
-
-/**
- * @typedef {object} Message
- *  @property {User} user - The user who sent the message
- *  @property {string} content - The content of the message
- */
-
-/**
- * @typedef {object} Chat
- * @property {Array<User>} users - The array of users who participated in the chat.
- * @property {Array<Message>} messages - The messages array sent in the chat.
+ * @property {string} prompt - Текст запроса, с которым взаимодействует ИИ для создания ответа
  */
