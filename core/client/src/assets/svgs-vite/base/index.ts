d7eabaea9e56a18fbe395a22db79fdb78c51e30b
sv022 <ssvatkin04@gmail.com>
2024-11-13 14:45:36 +0300

    add: base assets for sign-in page

--- core/client/src/assets/svgs-vite/base/index.ts
+++ core/client/src/assets/svgs-vite/base/index.ts
@@ -1,3 +1,5 @@
 import Arrow from './arrow.svg?component'
+import ArrowLeft from './arrow-left.svg'
+import ErrorCircle from './error-circle.svg?component'
 
-export { Arrow }
+export { Arrow, ArrowLeft, ErrorCircle }
