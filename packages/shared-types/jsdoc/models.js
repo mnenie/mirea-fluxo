8b4d979823891dbc17908f416a7f16ba1d4f86c1
mnenie <1alexpeshkov@gmail.com>
2024-11-14 00:02:34 +0300

    chore!: update docs

--- packages/shared-types/jsdoc/models.js
+++ packages/shared-types/jsdoc/models.js
@@ -1,21 +1,20 @@
 /**
  * @typedef {object} User
- * @property {number} _id - Уникальный идентификатор пользователя
+ * @property {string} _id - Уникальный идентификатор пользователя
  * @property {string} email - Адрес электронной почты пользователя
  * @property {string} photoUrl - URL профиля пользователя
  * @property {string} password - Пароль пользователя
- * @property {'manager' | 'corporation'} role - Роль пользователя в системе, может быть менеджер или организация
- * @property {string} organization - Название организации пользователя
+ * @property {'manager' | 'corporation'} role - Роль пользователя в системе, может быть менеджер или человек с гк организации
+ * @property {string} organization - Название организации пользователя !только если статус "corporation"
  */
 
 /**
- * @typedef {object} Orders
- * @property {number} _id - Уникальный идентификатор заказа
+ * @typedef {object} Order
+ * @property {string} _id - Уникальный идентификатор заказа
  * @property {string} title - Название заказа
  * @property {string} content - Содержание заказа
  * @property {Date} date - Дата и время создания заказа
  * @property {'not verified' | 'in process' | 'closed'} status - Статус заказа, может быть не проверен, в процессе или закрыт
- * @property {string} email - Адрес электронной почты заказчика
  * @property {number} price - Цена заказа
  * @property {string} organization - Название организации, ответственной за заказ
  */
