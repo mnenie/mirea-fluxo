40cb80533ecbac18583c2937376e2d4e7ae8fd53
mnenie <1alexpeshkov@gmail.com>
2024-11-16 17:17:15 +0300

    fix: language composable without effect scope

--- core/client/src/composables/useLanguage.ts
+++ core/client/src/composables/useLanguage.ts
@@ -1,5 +1,5 @@
 import { useLocalStorage } from '@vueuse/core'
-import { effectScope, onScopeDispose, type Ref, watch } from 'vue'
+import { type Ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 interface Value {
@@ -8,35 +8,26 @@ interface Value {
 }
 
 export function useLanguage(values: Value[], language: Ref<string>) {
-  const scope = effectScope()
-
-  const storage = useLocalStorage<string | null>('i18n', null)
   const { locale } = useI18n()
+  const storage = useLocalStorage<string | null>('i18n', null)
 
-  scope.run(() => {
-    watch(
-      () => locale.value,
-      (newLocale) => {
-        const selectedOption = values.find(option => option.value === newLocale)
-        if (selectedOption) {
-          language.value = selectedOption.value
-        }
-      },
-      { immediate: true },
-    )
-    watch(
-      () => language.value,
-      (newLanguage) => {
-        const selectedOption = values.find(option => option.value === newLanguage)
-        if (selectedOption) {
-          locale.value = selectedOption.value
-          storage.value = selectedOption.value
-        }
-      },
-    )
-  })
+  const setLanguage = (value: string) => {
+    locale.value = value
+    storage.value = value
+  }
 
-  onScopeDispose(() => {
-    scope.stop()
-  })
+  watch(
+    () => locale.value,
+    (newLocale) => {
+      const selectedOption = values.find(option => option.value === newLocale)
+      if (selectedOption) {
+        language.value = selectedOption.value
+      }
+    },
+    { immediate: true },
+  )
+
+  return {
+    setLanguage,
+  }
 }
